df07392221d64d4bfb966fdce890b10e5fdb436b
Francisco <frodriguez25@ucol.mx>
2023-09-21 20:15:50 -0600

    Paco & Axl DB

--- databases/retrostepdb.sql
+++ databases/retrostepdb.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: retrostepdb
 -- ------------------------------------------------------
--- Server version	10.4.28-MariaDB
+-- Server version	10.4.28-MariaDB f
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
